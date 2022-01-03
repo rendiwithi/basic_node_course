176516da6533897f138c13354454e9629c1f03ea
rendiwithi <rendidwih2@gmail.com>
2022-01-03 09:09:51 +0700

    clean fundamental course

---
+++ 0-fundamental/15-chunked-emit.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer(function (req,res) {
+    const fileStream = fs.createReadStream('./content/big-file.txt', 'utf-8');
+    fileStream.on('open',()=>{
+        fileStream.pipe(res)
+    })
+    fileStream.on('error',(err)=>{
+        res.end(err)
+    })
+}).listen(8000)
